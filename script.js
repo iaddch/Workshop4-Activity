8d57ca0e6555d61c45c168767bb08cc81f16f48c
iaddch <iaddche@gmail.com>
2026-03-24 18:31:52 -0400

    pt 2

--- script.js
+++ script.js
@@ -50,7 +50,6 @@ clearBtn.addEventListener("click", () => {
 // ── TASKS: code below this line ────────────────────────────
 
 const multiply = (a,b) => a*b;
-let product;
 multiplyBtn.addEventListener("click", () => {
     const result = multiply(Number(num1.value), Number(num2.value));
     calcResult.textContent = 'Result: $(result)';
