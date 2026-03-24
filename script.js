89e1a0a59d4a567fe447a9bccc2157aa6f070586
iaddch <iaddche@gmail.com>
2026-03-24 18:30:53 -0400

    Added multiplication functionality + display

--- script.js
+++ script.js
@@ -48,3 +48,10 @@ clearBtn.addEventListener("click", () => {
 });
 
 // ── TASKS: code below this line ────────────────────────────
+
+const multiply = (a,b) => a*b;
+let product;
+multiplyBtn.addEventListener("click", () => {
+    const result = multiply(Number(num1.value), Number(num2.value));
+    calcResult.textContent = 'Result: $(result)';
+});
